ddaad3d7668b4582d0c14598928e82a0341dc1cd
Shazad Ishaque <shazad_ishaque@yahoo.co.uk>
2018-09-10 14:44:13 +0100

    Added BookList Index page

---
+++ CRUD_Razor_2_1/Pages/BookList/Index.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CRUD_Razor_2_1.Pages.BookList
+{
+    public class IndexModel : PageModel
+    {
+        public string someData { get; set; }
+
+        public void OnGet()
+        {
+            someData = "This is my first Property!";
+        }
+    }
+}
